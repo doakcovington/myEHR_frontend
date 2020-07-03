1eb2c86991ed347d3c603428f42554f08ecdf79e
doakcovington <doakcovington@protonmail.com>
2020-07-03 13:12:41 -0700

    Built out report-container

--- index.js
+++ index.js
@@ -9,7 +9,23 @@ document.addEventListener('DOMContentLoaded', () => {
 function getReports(){
     fetch(endPoint)
     .then(response => response.json())
-    .then(reports => {
+    .then(reports => { //gets the reports data 
         //console.log(reports)
+        reports.data.forEach(report => { //data is the object key for the array value
+            const reportMarkup = `
+                <div data-id=${report.id}>
+                    <p>${report.attributes.date}</p>
+                    <p>${report.attributes.time}</p>
+                    <p>${report.attributes.blood_pressure}</p>
+                    <p>${report.attributes.temperature}</p>
+                    <p>${report.attributes.pulse}</p>
+                    <p>${report.attributes.pain}</p>
+                    <p>${report.attributes.comments}</p>
+                    <button data-id=${report.id}>edit</button>
+                </div>
+                <br>`;
+                document.querySelector('#report-container').innerHTML += reportMarkup
+
+        })
     })
 }
