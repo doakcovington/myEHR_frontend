d02b88fabcb9c7891d9142957e43fb1a848b19cf
doakcovington <doakcovington@protonmail.com>
2020-07-03 14:22:42 -0700

    Built out record form

--- index.js
+++ index.js
@@ -3,28 +3,29 @@ const endPoint = "http://localhost:3000/api/v1/records"
 
 //load DOM
 document.addEventListener('DOMContentLoaded', () => {
-    getReports();
+    getRecords();
 });
 
-function getReports(){
+function getRecords(){
     fetch(endPoint)
     .then(response => response.json())
-    .then(reports => { //gets the reports data 
-        //console.log(reports)
-        reports.data.forEach(report => { //data is the object key for the array value
-            const reportMarkup = `
-                <div data-id=${report.id}>
-                    <p>${report.attributes.date}</p>
-                    <p>${report.attributes.time}</p>
-                    <p>${report.attributes.blood_pressure}</p>
-                    <p>${report.attributes.temperature}</p>
-                    <p>${report.attributes.pulse}</p>
-                    <p>${report.attributes.pain}</p>
-                    <p>${report.attributes.comments}</p>
-                    <button data-id=${report.id}>edit</button>
+    .then(records => { //gets the records data 
+        //console.log(records)
+        records.data.forEach(record => { //data is the object key for the array value
+            //debugger
+            const recordMarkup = `
+                <div data-id=${record.id}>
+                    <p>${record.attributes.date}</p>
+                    <p>${record.attributes.time}</p>
+                    <p>${record.attributes.blood_pressure}</p>
+                    <p>${record.attributes.temperature}</p>
+                    <p>${record.attributes.pulse}</p>
+                    <p>${record.attributes.pain}</p>
+                    <p>${record.attributes.comments}</p>
+                    <button data-id=${record.id}>edit</button>
                 </div>
                 <br>`;
-                document.querySelector('#report-container').innerHTML += reportMarkup
+                document.querySelector('#record-container').innerHTML += recordMarkup
 
         })
     })
