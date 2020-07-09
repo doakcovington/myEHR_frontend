d158d6ae1a16b0b7916405a71710f7cd022d9660
doakcovington <doakcovington@protonmail.com>
2020-07-09 13:34:40 -0700

    Built out constructor

--- src/record.js
+++ src/record.js
@@ -1,5 +1,13 @@
 console.log("record.js")
 
 class Record {
-    
+    constructor(recordData) {
+        debugger
+        this.id = recordData.id
+        this.temperature = recordData.temperature
+        this.pulse = recordData.pulse
+        this.pain = recordData.pain
+        this.comments = recordData.comments
+        this.chart = recordData.chart
+    }
 }
