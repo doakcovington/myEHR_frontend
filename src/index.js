e3e5f44b0c63df26d5adacaf6f1d45c8c0e46c6b
doakcovington <doakcovington@protonmail.com>
2020-07-11 11:53:32 -0700

    Changed querySelector

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ function getRecords(){
         records.data.forEach(record => { //data is the object key for the array value
             let newRecord = new Record(record.id, record.attributes) //creates new instance of Record class 
 
-            document.querySelector('#record-container').innerHTML += newRecord.renderRecord()
+            document.querySelector('#table-body').innerHTML += newRecord.renderRecord()
         })
     })
 }
@@ -51,7 +51,7 @@ function postRecord(temperature, pulse, pain, comments, chart_id) {
         const recordData = record.data
         let newRecord = new Record(recordData, recordData.attributes) //creates new instance of Record class 
 
-        document.querySelector('#record-container').innerHTML += newRecord.renderRecord()
+        document.querySelector('#table-body').innerHTML += newRecord.renderRecord()
     })
     // .catch((error) => {
     //     console.log(error);
