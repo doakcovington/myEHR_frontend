0cf5076c481e9a53758fbc2a9c3f4f52fe91382b
doakcovington <doakcovington@protonmail.com>
2020-07-28 19:12:05 -0700

    Formatted date

--- src/record.js
+++ src/record.js
@@ -14,7 +14,7 @@ class Record {
     renderRecord(){
         return `
                 <tr data-record-id=${this.id}>
-                    <td class="text-center">${this.created_at}</td>
+                    <td class="text-center">${formatDate(this.created_at)}</td>
                     <td class=${this.temperature >= 99 ? "bg-danger" : "text-center"}>${this.temperature}</td>
                     <td class="text-center">${this.pulse}</td>
                     <td class="text-center">${this.pain}
@@ -23,6 +23,20 @@ class Record {
                 </tr>
             `;
     }
+
+    formatDate(date){
+        let newDate = new Date(date),
+        month = '' + (newDate.getMonth() + 1),
+        day = '' + newDate.getDate(),
+        year = newDate.getFullYear();
+    
+        if(month.length < 2)
+            month = '0' + month;
+        if(day.length < 2)
+            day = '0' + day;
+        return [year, month, day].join('/');
+    }
+    
 
     static findById(id) {
         return this.all.find(record => record.id === id);
