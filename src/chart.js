8437243db2f1962d8f473139d6ede77105a861a3
doakcovington <doakcovington@protonmail.com>
2020-08-05 11:06:47 -0700

    Built out renderChart

--- src/chart.js
+++ src/chart.js
@@ -7,4 +7,14 @@ class Chart {
         this.pcp = pcp;
         this.dob = dob;
     }
+
+    renderChart() {
+        return `
+            <div class="row" data-record-id=${this.id}>
+            <div class="col-sm-4" style="background-color:lavender;">Name: ${this.name}</div>
+            <div class="col-sm-4" style="background-color:lavenderblush;">Date of Birth: ${formatDate(this.dob)}</div>
+            <div class="col-sm-4" style="background-color:lavender;">Primary Care Provider: ${this.pcp}</div>
+            </div>
+        `
+    }
 }
