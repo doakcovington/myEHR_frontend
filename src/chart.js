253b94a7a6590262ffd02c341998d2a06f2cc240
doakcovington <doakcovington@protonmail.com>
2020-08-05 11:03:20 -0700

    Built out constructor

---
+++ src/chart.js
@@ -0,0 +1,10 @@
+console.log("chart.js");
+
+class Chart {
+    constructor(id, chartAttributes) {
+        this.id = id;
+        this.name = name;
+        this.pcp = pcp;
+        this.dob = dob;
+    }
+}
