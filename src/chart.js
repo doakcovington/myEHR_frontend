8d5a33eb9f042492f73de1af598171e3bd267fca
doakcovington <doakcovington@protonmail.com>
2020-08-05 14:50:06 -0700

    Removed inline styling

--- src/chart.js
+++ src/chart.js
@@ -12,9 +12,9 @@ class Chart {
     renderChart() {
         return `
             <div class="row" data-record-id=${this.id}>
-            <div class="col-sm-4" style="background-color:lavender;">Name: ${this.name}</div>
-            <div class="col-sm-4" style="background-color:lavenderblush;">Date of Birth: ${formatDate(this.dob)}</div>
-            <div class="col-sm-4" style="background-color:lavender;">Primary Care Provider: ${this.pcp}</div>
+            <div class="col-sm-4">Name: ${this.name}</div>
+            <div class="col-sm-4">Date of Birth: ${formatDate(this.dob)}</div>
+            <div class="col-sm-4">Primary Care Provider: ${this.pcp}</div>
             </div>
         `
     }
